e63b14d380880e7e6d242abdadbbb2d74ae9905e
MishC <michaela.chovancova@gmail.com>
2021-11-04 13:13:54 +0100

    searchEngine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-function App() {
+export default function App() {
+  let [keyword, setKeyword] = useState("");
+  function handleSearchWord(event) {
+    event.preventDefault();
+    setKeyword(event.target.value);
+  }
+  function search(event) {
+    event.preventDefault();
+    alert(`You are looking for ${keyword}`);
+  }
   return (
     <div className="App">
-      <header className="App-header">Hello!</header>
+      <header className="App-header">
+        <form onSubmit={search}>
+          <div className="inline">
+            <input
+              type="search"
+              placeholder="Search a word"
+              className="form-control shadow-sm"
+              autoFocus="on"
+              autoComplete="off"
+              onChange={handleSearchWord}
+            />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary btn-rounded shadow-sm "
+            />
+          </div>
+        </form>
+      </header>
+      <main></main>
     </div>
   );
 }
-
-export default App;
